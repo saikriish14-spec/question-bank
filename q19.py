c7e0de12b17e7c51013c809fc4849fe57e5f4bff
saikriish14-spec <saikriish14@gmail.com>
2026-09-16 16:45:58 +0100

    ok

---
+++ q19.py
@@ -0,0 +1,12 @@
+# Defined password placeholder
+correct_password = "Sai"
+
+
+user_input = input("Enter the password: ")
+
+if user_input == correct_password:
+    print("Access Granted")
+else:
+    print("Access Denied")
+
+
